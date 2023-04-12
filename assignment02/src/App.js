82af6b7d97fd57cbf652e893bbffdb9f683b12c1
Ethan Kinneer <ethan@kinneer5.com>
2023-04-11 20:12:10 -0500

    Updates
    Added json, code for Cart view is in App.js

--- assignment02/src/App.js
+++ assignment02/src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+const listItems = items.map((el) => (
+  // PRODUCT
+  <div class="row border-top border-bottom" key={el.id}>
+    <div class="row main align-items-center">
+      <div class="col-2">
+        <img class="img-fluid" src={el.image} />
+      </div>
+      <div class="col">
+        <div class="row text-muted">{el.title}</div>
+        <div class="row">{el.category}</div>
+      </div>
+      <div class="col">
+        <button
+          type="button"
+          variant="light"
+          onClick={() => removeFromCart(el)}
+        >
+          {" "}
+          -{" "}
+        </button>{" "}
+        <button type="button" variant="light" onClick={() => addToCart(el)}>
+          {" "}
+          +{" "}
+        </button>
+      </div>
+      <div class="col">
+        ${el.price} <span class="close">&#10005;</span>
+        {howManyofThis(el.id)}
+      </div>
+    </div>
+  </div>
+));
+
+function howManyofThis(id) {
+  let hmot = cart.filter((cartItem) => cartItem.id === id);
+  return hmot;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      STORE SE/ComS319
+      <div class="card">
+        <div class="row">
+          {/* HERE, IT IS THE SHOPING CART */}
+          <div class="col-md-8 cart">
+            <div class="title">
+              <div class="row">
+                <div class="col">
+                  <h4>
+                    <b>319 Shopping Cart</b>
+                  </h4>
+                </div>
+                <div class="col align-self-center text-right text-muted">
+                  Products selected {cart.length}
+                </div>
+              </div>
+            </div>
+            <div>{listItems}</div>
+          </div>
+          <div class="float-end">
+            <p class="mb-0 me-5 d-flex align-items-center">
+              <span class="small text-muted me-2">Order total:</span>
+              <span class="lead fw-normal">${cartTotal}</span>
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
-
 export default App;
